f55f57d8590e2b369e2007a67420625355a09ebf
Chirath02 <chirath.02@gmail.com>
2017-01-20 21:18:45 +0530

    completed setup() in motor control

--- control.py
+++ control.py
@@ -1,33 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
 
-# select the pins
-servoPin = 3
-sp = 11
-in1 = 5
-in2 = 7
-
-GPIO.setup(servoPin, GPIO.OUT)
-
-GPIO.setup(sp, GPIO.OUT)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-
-pwm = GPIO.PWM(servoPin, 50)
-
-speed = GPIO.PWM(sp, 50)
-
-speed.start(100)
-
-GPIO.output(in1, 1)
-
-
+def setUp:
+    GPIO.setmode(GPIO.BOARD)
+    # select the pins
+    servoPin = 3
+    sp = 11
+    in1 = 5
+    in2 = 7
+    # set pins as output
+    GPIO.setup(servoPin, GPIO.OUT)
+    GPIO.setup(sp, GPIO.OUT)
+    GPIO.setup(in1, GPIO.OUT)
+    GPIO.setup(in2, GPIO.OUT)
+    pwm = GPIO.PWM(servoPin, 50)
+    speed = GPIO.PWM(sp, 50)
+    speed.start(30)
+    pwm.start(0)
 
 pwm.start(7)  #center
 time.sleep(2)
-pwm.ChangeDutyCycle(5) #right 
+pwm.ChangeDutyCycle(5) #right
 time.sleep(2)
 pwm.ChangeDutyCycle(9) #left
 time.sleep(2)
