db9cafc6ac0a7a1334312c558742fed96bba49ef
Chirath02 <chirath.02@gmail.com>
2017-02-22 00:13:00 +0530

    added motor controls

--- client.py
+++ client.py
@@ -5,10 +5,11 @@ from motor.motorControl import Motor
 client = Client()
 motor = Motor()
 
-c = client.connect(12345)
+c = client.connect(5000)
 
 while 1:
     print(c.recv(1024))
+    motor.input(c)
 
 
 c.close()
