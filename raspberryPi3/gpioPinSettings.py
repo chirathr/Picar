aafa11cd9db2c734f76e4eae653a46101240c18b
Chirath R <chirath.02@gmail.com>
2017-10-17 21:09:41 +0530

    added the default pins

---
+++ raspberryPi3/gpioPinSettings.py
@@ -0,0 +1,25 @@
+# Created by Chirath R <chirath.02@gmail.com>
+# This file contains all the gpio pins used in this project.
+
+
+# Ultrasonic sensor pins
+
+ECHO_PIN = 23                                         # Associate pin 23 to ECHO
+
+FRONT_TRIGGER = 13                                    # Front trigger pin
+BACK_TRIGGER = 19                                     # Back trigger pin
+
+LEFT_TRIGGER = 21                                     # Left trigger pin
+RIGHT_TRIGGER = 15                                    # Right trigger pin
+
+# Servo control pins
+
+SERVO = 3
+
+# motor control pins
+
+IN1 = 7
+IN2 = 5
+SPEED = 11
+
+# lights control pins
