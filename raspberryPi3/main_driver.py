658fbc2a0efc5623a7992ccd15b1ef1973976707
akshayharidas <akshayharidas006@gmail.com>
2017-12-17 04:55:25 +0000

    comment in main driver'

--- raspberryPi3/main_driver.py
+++ raspberryPi3/main_driver.py
@@ -1,9 +1,12 @@
 from camera.video_stream import VideoStream
 from motor.motor_controller import MotorController
 
-vs = VideoStream('192.168.43.49', 8001)
+
 mc = MotorController('192.168.43.49', 8000)
-vs.start()
+vs = VideoStream('192.168.43.49', 8001)
+
 mc.start()
-vs.join()
+#vs.start()
+
 mc.join()
+#vs.join()
