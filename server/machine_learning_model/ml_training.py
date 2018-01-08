ef6825ae91c2c954f045cb886641299350e1715e
Chirath R <chirath.02@gmail.com>
2018-01-08 12:15:55 +0530

    ml_traing with 2 output

--- server/machine_learning_model/ml_training.py
+++ server/machine_learning_model/ml_training.py
@@ -8,76 +8,9 @@ import os
 class MLModel(object):
     def __init__(self):
         self.image_array = np.zeros((1, 38400), dtype=np.float32)
-        self.label_array = np.zeros((1, 4), dtype=np.float32)
-        self.image_data = np.zeros((1, 38400), dtype=np.float32)
+        self.label_array = np.zeros((1, 2), dtype=np.float32)
 
         self.training_data_base_url = '../training_data/'
-
-    def load_training_data_image(self, file_name):
-        """
-            This function takes all the images from the image path and converts
-            to an numpy float32 array. It also loads the saved label data.
-        """
-        # start timer
-        t0 = cv2.getTickCount()
-
-        # load all the label data
-        self.label_array = np.load(self.training_data_base_url + 'label_data/' + file_name + '.npz')['train_labels']
-
-        # get all images
-
-        images_files = glob.glob(self.training_data_base_url + 'image_data/' + file_name + '/*.jpg')
-
-        # convert all images to numpy float32 array and stack them
-        for image in images_files:
-            # read an image
-            img = cv2.imread(image, 0)
-
-            # get the lower half of the image
-            roi = img[120:240, :]
-
-            # convert img to a 1D array
-            temp_array = roi.reshape(1, 38400).astype(np.float32)
-
-            # stack image into rows
-            self.image_data = np.vstack((self.image_data, temp_array))
-
-        self.image_array = self.image_data[1:-1]
-        self.label_array = self.label_array[1:]
-
-        print self.image_array.shape
-        print self.label_array.shape
-
-        # end timer
-        t1 = cv2.getTickCount()
-
-        # print the time to load the image
-        time = (t1 - t0) / cv2.getTickFrequency()
-        print 'Image loaded in :', time
-
-    def load_training_data(self, file_name):
-        # start timer
-        t0 = cv2.getTickCount()
-
-        # load all the label data
-        self.label_array = np.load(self.training_data_base_url + 'label_data/' + file_name + '.npz')['train_labels']
-
-        self.image_array = np.load(self.training_data_base_url + 'image_data/img-' + file_name + '.npz')['image_data']
-
-        print (self.training_data_base_url + 'image_data/img-' + file_name + '.npz')
-
-        self.label_array = self.label_array[1:]
-
-        print self.image_array.shape
-        print self.label_array.shape
-
-        # end timer
-        t1 = cv2.getTickCount()
-
-        # print the time to load the image
-        time = (t1 - t0) / cv2.getTickFrequency()
-
-        print 'Image loaded in :', time
 
     def load_all_training_data(self):
         # start timer
@@ -88,15 +21,18 @@ class MLModel(object):
         label_files = glob.glob(self.training_data_base_url + 'label_data/*.npz')
 
         for label_file in label_files:
-            self.label_array = np.vstack((self.label_array, np.load(label_file)['train_labels'][1:]))
+            label_data = np.load(label_file)['train_labels'][1:]
+            for label in label_data:
+                label_data = np.zeros((1, 2), dtype=np.float32)
+                label_data[0][0] = label[3]
+                label_data[0][1] = label[1]
+                self.label_array = np.vstack((self.label_array, label_data))
+
             self.image_array = np.vstack((self.image_array, np.load(
                 self.training_data_base_url + 'image_data/img-' + label_file[-11:])['image_data']))
             print (label_file)
             print self.image_array.shape
             print self.label_array.shape
-
-        self.image_array = self.image_array[1:]
-        self.label_array = self.label_array[1:]
 
         print self.image_array.shape
         print self.label_array.shape
@@ -120,20 +56,17 @@ class MLModel(object):
 
         # Train method as
         model.setTrainMethod(cv2.ml.ANN_MLP_RPROP | cv2.ml.ANN_MLP_UPDATE_WEIGHTS)
-        model.setLayerSizes(np.int32([38400, 32, 4]))
+        model.setLayerSizes(np.int32([38400, 32, 2]))
         model.setActivationFunction(cv2.ml.ANN_MLP_SIGMOID_SYM)
         model.setTermCriteria((cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 500, 0.0001))
 
-        if len(sys.argv) == 2:
-            self.load_training_data(sys.argv[1])
-        else:
-            self.load_all_training_data()
+        self.load_all_training_data()
 
         mlp_file = glob.glob('./mlp_xml/*.xml')
 
         if len(mlp_file) > 0:
             print ('MLP data already found: ' + mlp_file[0])
-            model = model.load(mlp_file[0])
+            model = cv2.ml.ANN_MLP_load(mlp_file[0])
             print ('IsTrained : ' + str(model.isTrained()))
         else:
             if not os.path.exists('./mlp_xml/'):
