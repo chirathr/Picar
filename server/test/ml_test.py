45074378ab1b39b0385c9898ac4c5375bece1fad
Chirath R <chirath.02@gmail.com>
2018-01-09 22:07:02 +0530

    updated test

--- server/test/ml_test.py
+++ server/test/ml_test.py
@@ -6,6 +6,7 @@ import sys
 def check(prediction, label_truth):
 
     print (prediction, label_truth)
+    prediction = int(prediction)
 
     if 3 > prediction >= 0:
         if label_truth[prediction] == 1:
@@ -19,15 +20,14 @@ if len(sys.argv) != 2:
     print ('Usage: %s data00x' % __file__)
     sys.exit(0)
 else:
-    model = cv2.ml.ANN_MLP_create()
-    model = model.load('../machine_learning_model/mlp_xml/mlp.xml')
+    model = cv2.ml.ANN_MLP_load('../machine_learning_model/mlp_xml/mlp.xml')
 
     image_data = numpy.load('../training_data/image_data/img-' + sys.argv[1] + '.npz')['image_data']
-    label_data = numpy.load('../training_data/label_data/' + sys.argv[1] + '.npz')['train_labels'][:-1]
+    label_data = numpy.load('../training_data/label_data/' + sys.argv[1] + '.npz')['label_data'][:-1]
 
     correct = 0
 
-    for i in range(len(image_data)):
+    for i in range(len(label_data)):
         output = model.predict(image_data[i:i+1])[0]
         correct += check(output, label_data[i].tolist())
 
